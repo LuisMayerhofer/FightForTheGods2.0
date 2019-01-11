747ee1b40cff530e0beb2d2c9667c9ee737c4ff3
Bobo DerTitan <iris.mayerhofer@gmx.de>
2019-01-11 18:59:35 +0100

    .

---
+++ Fight for the Gods/Assets/Scripts/Player1Scr/Player1Ultimate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Ultimate : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private Animator anim;
+    private SpriteRenderer sr;
+
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("e") || Input.GetButtonDown("Y Button"))
+        {
+
+        }
+    }
+}
---
+++ Fight for the Gods/Assets/Scripts/ZauberScr/Pentagramm.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pentagramm : MonoBehaviour
+{
+    public float lifetime;
+    void Start()
+    {
+        Invoke("Die", lifetime);
+    }
+
+   void Die()
+    {
+        Destroy(gameObject);
+    }
+}
