50e24d01eec312be26d731b077e6c9881ac386e0
LuisMayerhofer <46067023+LuisMayerhofer@users.noreply.github.com>
2019-02-10 14:26:32 +0100

    addded SteinBall(unfertig)

---
+++ Fight for the Gods/Assets/Scripts/Leveldesign/SteinBallScr.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteinBallScr : MonoBehaviour
+{
+    public int damage;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Shield"))
+        {
+            Destroy(col.gameObject);
+        }
+        else if (col.CompareTag("Player1") || col.CompareTag("Player2"))
+        {
+            col.SendMessageUpwards("GetDamage", damage);
+        }
+    }
+
+}
